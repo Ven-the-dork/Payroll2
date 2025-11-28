5ca811cec7d6429130ae171d94e76332b81e0ff3
Ven-the-dork <reaven.miano@cvsu.edu.ph>
2025-11-28 17:39:41 +0800

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// Replace with your actual Firebase config from Step 2
+
 const firebaseConfig = {
   apiKey: "AIzaSyCQxzmA4KyslCDEB_J3G9fvpvpDfY6vdk8",
   authDomain: "cvsu-payroll.firebaseapp.com",
@@ -11,8 +11,9 @@ const firebaseConfig = {
   appId: "1:607770244208:web:20193ea851c41096f130b5"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase Authentication and export it
+
+
 export const auth = getAuth(app);
